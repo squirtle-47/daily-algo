0afdcc0ece217f786e27455354d97150420a7626
samflam <me@samflam.com>
2022-01-24 16:24:00 -0800

    Initial redux setup

---
+++ client/App.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import AlgoContainer from './containers/AlgoContainer.jsx';
+
+const App = () => {
+  return <AlgoContainer />;
+}
+export default App;
--- client/index.js
+++ client/index.js
@@ -1,7 +1,14 @@
 import React from "react";
 import { render } from "react-dom";
-import App from "./containers/App.jsx";
+import { Provider } from 'react-redux';
+import App from "./App.jsx";
+import store from './store';
 
 import style from "./scss/application.scss";
 
-render(<App />, document.getElementById("root"));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
